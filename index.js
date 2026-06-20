4c3e595edeb68f46bc12a5d9d888dd06a6a8f206
Mostakim <mostakim2005arafat@gmail.com>
2026-06-20 18:31:55 +0600

    delete function comment

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const dns = require('node:dns')
-dns.setServers(['8.8.8.8','8.8.4.4'])
+dns.setServers(['8.8.8.8', '8.8.4.4'])
 const express = require('express')
 const app = express()
 const port = 5000
@@ -11,11 +11,11 @@ app.use(cors());
 app.use(express.json())
 
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 const myDB = client.db("ideas_vault");
@@ -23,70 +23,87 @@ const myColl = myDB.collection("Ideas");
 const myCollComment = myDB.collection('Comment')
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
 
-    app.post('/ideas', async(req,res) => {
-        const data =  await req.body
-        const result = await myColl.insertOne(data)
-        res.send(result)
-    })
+        app.post('/ideas', async (req, res) => {
+            const data = await req.body
+            const result = await myColl.insertOne(data)
+            res.send(result)
+        })
 
-    app.get('/ideas', async(req,res) => {
-        const result = await myColl.find().toArray()
-        res.send(result)
-    })
+        app.get('/ideas', async (req, res) => {
+            const result = await myColl.find().toArray()
+            res.send(result)
+        })
 
-    app.get('/ideas/:id', async(req,res) => {
-        const idnumber = req.params.id
-        // console.log(idnumber)
-        const querys = {
-            _id : new ObjectId(idnumber)
-        }
-        const result = await myColl.findOne(querys)
-        res.send(result)
-    })
+        app.get('/ideas/:id', async (req, res) => {
+            const idnumber = req.params.id
+            // console.log(idnumber)
+            const querys = {
+                _id: new ObjectId(idnumber)
+            }
+            const result = await myColl.findOne(querys)
+            res.send(result)
+        })
 
-    app.post('/comment', async(req,res) => {
-        const body = await req.body
-        if(body.Data){
-        const result = await myCollComment.insertOne(body)
-        res.send(result)
-        }
-    })
+        app.post('/comment', async (req, res) => {
+            const body = await req.body
+            if (body.Data) {
+                const result = await myCollComment.insertOne(body)
+                res.send(result)
+            }
+        })
 
-    // app.patch('/:userID/comment',async(req,res) => {
+        app.patch('/comment', async (req, res) => {
+            const bodyTime = req.body.Time
+            const bodysData = req.body.Data
+            const bodysID = req.body.Commentid
+            console.log(req.body)
+            const query = { _id : new ObjectId(bodysID) };
+            const update = { $set: { Data: bodysData , Time : bodyTime } };
+            const options = { };
+            const result = await myCollComment.updateOne(query, update, options);
+            //console.log(result)
+            res.send(result)
+        })
 
-    // })
+        app.delete('/comment', async(req,res) => {
+            const bodysID = req.body.commentID
+            const qureys = {_id : new ObjectId(bodysID)}
+            const result = await myCollComment.deleteOne(qureys)
+            console.log(result)
+            res.send(result)
+        })
 
-    app.get('/ideas/:id/comment', async(req,res) => {
-        const idorginal = req.params.id
-        console.log(idorginal)
-        const querys ={
-            IdeaPageid : idorginal
-        }
-        const result = await myCollComment.find(querys).toArray()
-        // console.log(result)
-        res.send(result)
-    })
+        app.get('/ideas/:id/comment', async (req, res) => {
+            const idorginal = req.params.id
+            console.log(idorginal)
+            const querys = {
+                IdeaPageid: idorginal
+            }
+            const result = await myCollComment.find(querys).toArray()
+            // console.log(result)
+            res.send(result)
+        })
 
-    // Send a ping to confirm a successful connection
-    await client.db("ideas_vault").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    //await client.close();
-  }
+        // Send a ping to confirm a successful connection
+        await client.db("ideas_vault").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        //await client.close();
+    }
 }
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    res.send('Hello World!');
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
 
